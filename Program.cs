aba45cc72681d2afb7fe2079895100ed0cd807dc
Oleg Iraklionov <irakliov@mail.ru>
2022-12-20 17:42:30 +0300

    Case6 is ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программубкоторая определяет четность числа
+
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
